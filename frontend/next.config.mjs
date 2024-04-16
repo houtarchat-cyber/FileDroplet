64ce1bd0f0a64dbeac1ad7d6fe79a00784134cb7
houtar1121 <160732918+houtar1121@users.noreply.github.com>
2024-04-16 14:10:37 +0000

    Add webpack configuration and update dependencies

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias.canvas = false;
+        // Important: return the modified config
+        config.module.rules.push({
+            test: /\.node/,
+            use: 'raw-loader',
+        });
+        return config;
+    },
+}
 
 export default nextConfig;
