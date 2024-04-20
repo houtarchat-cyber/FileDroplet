acde3b9a3954aff66032809b262d0e0c4d4f619e
houtar1121 <160732918+houtar1121@users.noreply.github.com>
2024-04-20 09:39:31 +0000

    Update layout and page components

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,23 @@
-import type { Metadata } from "next";
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "FileDroplet",
-  description: "一个极简、快速和安全的文件分享和图片托管平台。",
+const Dynamic = ({ children }: { children: React.ReactNode }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <>{children}</>;
 };
 
 export default function RootLayout({
@@ -16,7 +27,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#11152A" />
+        <meta name="description" content="一个极简、快速和安全的文件分享和图片托管平台。无需注册登录。上传文件，分享链接，即刻开始。" />
+        <meta name="keywords" content="文件分享, 文件托管, 图片分享, 图片托管, 文件上传, 图片上传" />
+        <title>FileDroplet</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta name="msapplication-TileColor" content="#11152A" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      </head>
+      <body className={inter.className}>
+        <Dynamic>{children}</Dynamic>
+      </body>
     </html>
   );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,50 +1,56 @@
 'use client';
 
-import { Link, HashRouter as Router, Route, Routes } from "react-router-dom"
-import Image from "next/image"
-import Navbar from "@/components/Navbar"
-import ImageUploader from "@/components/ImageUploader"
-import FileUploader from "@/components/FileUploader"
-import FeedbackPage from "@/components/Feedback"
-import FileViewer from "@/components/FileViewer"
-import CollectionViewer from "@/components/CollectionViewer"
-import Footer from "@/components/Footer"
-import Icon from "@/app/icon.webp"
+import CollectionViewer from "@/components/CollectionViewer";
+import FeedbackPage from "@/components/Feedback";
+import FileUploader from "@/components/FileUploader";
+import FileViewer from "@/components/FileViewer";
+import Footer from "@/components/Footer";
+import ImageUploader from "@/components/ImageUploader";
+import Navbar from "@/components/Navbar";
+import { store, persistor } from "@/store";
+import { Provider } from "react-redux";
+import { Link, Route, HashRouter as Router, Routes } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 
 export default function Page() {
   return (
     <Router>
-      <div className="flex flex-col h-screen">
-        <header className="flex items-center h-16 border-b p-4">
-          <div className="flex items-center flex-1">
-            <Image src={Icon} alt="FileDroplet" className="h-14 w-14 mr-2" />
-            <Link
-              to="/"
-              className="text-xl font-semibold" 
-              style={{ 
-                transition: 'text-shadow 0.2s ease', 
-                textShadow: 'none' 
-              }}
-              onMouseOver={(event) => event.currentTarget.style.textShadow = '0 5px 15px rgba(0,0,0,0.3)'}
-              onMouseOut={(event) => event.currentTarget.style.textShadow = 'none'}
-            >
-              FileDroplet
-            </Link>
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={null}>
+          <div className="flex flex-col h-screen">
+            <header className="flex items-center h-16 border-b p-4">
+              <div className="flex items-center flex-1">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src="/icon.webp" alt="FileDroplet" className="h-14 w-14 mr-2" />
+                <Link
+                  to="/"
+                  className="text-xl font-semibold" 
+                  style={{ 
+                    transition: 'text-shadow 0.2s ease', 
+                    textShadow: 'none' 
+                  }}
+                  onMouseOver={(event) => event.currentTarget.style.textShadow = '0 5px 15px rgba(0,0,0,0.3)'}
+                  onMouseOut={(event) => event.currentTarget.style.textShadow = 'none'}
+                >
+                  FileDroplet
+                </Link>
+              </div>
+              <Navbar />
+            </header>
+            <main className="flex-1 flex flex-col items-center justify-center p-4 gap-4">
+              <Routes>
+                <Route path="/" element={<FileUploader />} />
+                <Route path="/feedback" element={<FeedbackPage />} />
+                <Route path="/image" element={<ImageUploader />} />
+                <Route path="/files/:id" element={<FileViewer />} />
+                <Route path="/collections/:id" element={<CollectionViewer />} />
+              </Routes>
+            </main>
+            <Footer />
           </div>
-          <Navbar />
-        </header>
-        <main className="flex-1 flex flex-col items-center justify-center p-4 gap-4">
-          <Routes>
-            <Route path="/" element={<FileUploader />} />
-            <Route path="/feedback" element={<FeedbackPage />} />
-            <Route path="/image" element={<ImageUploader />} />
-            <Route path="/files/:id" element={<FileViewer />} />
-            <Route path="/collections/:id" element={<CollectionViewer />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+        </PersistGate>
+      </Provider>
     </Router>
   )
 }
